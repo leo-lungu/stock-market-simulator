abb403e7e29e79b28e7518cfc3b00746d50c8856
Leo <95043714+leo-lungu@users.noreply.github.com>
2022-04-21 20:31:49 +0100

    Delete LowRiskStocks.java
    No longer in use.

--- LowRiskStocks.java
+++
@@ -1,9 +0,0 @@
-public class LowRiskStocks extends Stocks{
-    double interest;
-    double differentTrades;
-    public LowRiskStocks(String ticker, double currentPrice) {
-        super(ticker, currentPrice);
-        this.interest =  1.00025;
-        this.differentTrades = 0.0025;
-    }
-}
